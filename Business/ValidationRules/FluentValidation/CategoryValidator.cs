82ea2d95ad04d78038b12ed735b308db33bcf6e4
Rıdvan <ridvanabay@gmail.com>
2021-05-25 17:30:17 +0300

    Added FluentValidation Added BlogValidator Added CategoryValidator

---
+++ Business/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using YazilimBlogApp.Entities.Concrete;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+
+        }
+    }
+}
