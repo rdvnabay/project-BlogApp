7b1a6d80c9c825a72fe8113ca2490c1f7e14abad
Rıdvan <ridvanabay@gmail.com>
2021-05-25 16:47:58 +0300

    added Core Layer - Results

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        public Result(bool success,string message):this(success)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+        public bool Success { get; set; }
+    }
+}
